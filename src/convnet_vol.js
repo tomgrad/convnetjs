726771b75456d09c67a877b7c7b0783b8be626dc
Tomasz Gradowski <tomgrad@gmail.com>
2026-09-16 01:16:40 +0200

    refactor: convert Vol and vol utils to classes and modern syntax

--- src/convnet_vol.js
+++ src/convnet_vol.js
@@ -7,104 +7,103 @@ import { zeros, randn } from './convnet_util.js';
   // all weights, and also stores all gradients w.r.t. 
   // the data. c is optionally a value to initialize the volume
   // with. If c is missing, fills the Vol with random numbers.
-  var Vol = function(sx, sy, depth, c) {
-    // this is how you check if a variable is an array. Oh, Javascript :)
-    if(Object.prototype.toString.call(sx) === '[object Array]') {
-      // we were given a list in sx, assume 1D volume and fill it up
-      this.sx = 1;
-      this.sy = 1;
-      this.depth = sx.length;
-      // we have to do the following copy because we want to use
-      // fast typed arrays, not an ordinary javascript array
-      this.w = zeros(this.depth);
-      this.dw = zeros(this.depth);
-      for(var i=0;i<this.depth;i++) {
-        this.w[i] = sx[i];
-      }
-    } else {
-      // we were given dimensions of the vol
-      this.sx = sx;
-      this.sy = sy;
-      this.depth = depth;
-      var n = sx*sy*depth;
-      this.w = zeros(n);
-      this.dw = zeros(n);
-      if(typeof c === 'undefined') {
-        // weight normalization is done to equalize the output
-        // variance of every neuron, otherwise neurons with a lot
-        // of incoming connections have outputs of larger variance
-        var scale = Math.sqrt(1.0/(sx*sy*depth));
-        for(var i=0;i<n;i++) { 
-          this.w[i] = randn(0.0, scale);
+  class Vol {
+    constructor(sx, sy, depth, c) {
+      // this is how you check if a variable is an array. Oh, Javascript :)
+      if(Array.isArray(sx)) {
+        // we were given a list in sx, assume 1D volume and fill it up
+        this.sx = 1;
+        this.sy = 1;
+        this.depth = sx.length;
+        // we have to do the following copy because we want to use
+        // fast typed arrays, not an ordinary javascript array
+        this.w = zeros(this.depth);
+        this.dw = zeros(this.depth);
+        for(let i=0;i<this.depth;i++) {
+          this.w[i] = sx[i];
         }
       } else {
-        for(var i=0;i<n;i++) { 
-          this.w[i] = c;
+        // we were given dimensions of the vol
+        this.sx = sx;
+        this.sy = sy;
+        this.depth = depth;
+        const n = sx*sy*depth;
+        this.w = zeros(n);
+        this.dw = zeros(n);
+        if(c === undefined) {
+          // weight normalization is done to equalize the output
+          // variance of every neuron, otherwise neurons with a lot
+          // of incoming connections have outputs of larger variance
+          const scale = Math.sqrt(1.0/(sx*sy*depth));
+          for(let i=0;i<n;i++) { 
+            this.w[i] = randn(0.0, scale);
+          }
+        } else {
+          for(let i=0;i<n;i++) { 
+            this.w[i] = c;
+          }
         }
       }
     }
-  }
 
-  Vol.prototype = {
-    get: function(x, y, d) { 
-      var ix=((this.sx * y)+x)*this.depth+d;
+    get(x, y, d) { 
+      const ix=((this.sx * y)+x)*this.depth+d;
       return this.w[ix];
-    },
-    set: function(x, y, d, v) { 
-      var ix=((this.sx * y)+x)*this.depth+d;
+    }
+    set(x, y, d, v) { 
+      const ix=((this.sx * y)+x)*this.depth+d;
       this.w[ix] = v; 
-    },
-    add: function(x, y, d, v) { 
-      var ix=((this.sx * y)+x)*this.depth+d;
+    }
+    add(x, y, d, v) { 
+      const ix=((this.sx * y)+x)*this.depth+d;
       this.w[ix] += v; 
-    },
-    get_grad: function(x, y, d) { 
-      var ix = ((this.sx * y)+x)*this.depth+d;
+    }
+    get_grad(x, y, d) { 
+      const ix = ((this.sx * y)+x)*this.depth+d;
       return this.dw[ix]; 
-    },
-    set_grad: function(x, y, d, v) { 
-      var ix = ((this.sx * y)+x)*this.depth+d;
+    }
+    set_grad(x, y, d, v) { 
+      const ix = ((this.sx * y)+x)*this.depth+d;
       this.dw[ix] = v; 
-    },
-    add_grad: function(x, y, d, v) { 
-      var ix = ((this.sx * y)+x)*this.depth+d;
+    }
+    add_grad(x, y, d, v) { 
+      const ix = ((this.sx * y)+x)*this.depth+d;
       this.dw[ix] += v; 
-    },
-    cloneAndZero: function() { return new Vol(this.sx, this.sy, this.depth, 0.0)},
-    clone: function() {
-      var V = new Vol(this.sx, this.sy, this.depth, 0.0);
-      var n = this.w.length;
-      for(var i=0;i<n;i++) { V.w[i] = this.w[i]; }
+    }
+    cloneAndZero() { return new Vol(this.sx, this.sy, this.depth, 0.0)}
+    clone() {
+      const V = new Vol(this.sx, this.sy, this.depth, 0.0);
+      const n = this.w.length;
+      for(let i=0;i<n;i++) { V.w[i] = this.w[i]; }
       return V;
-    },
-    addFrom: function(V) { for(var k=0;k<this.w.length;k++) { this.w[k] += V.w[k]; }},
-    addFromScaled: function(V, a) { for(var k=0;k<this.w.length;k++) { this.w[k] += a*V.w[k]; }},
-    setConst: function(a) { for(var k=0;k<this.w.length;k++) { this.w[k] = a; }},
+    }
+    addFrom(V) { for(let k=0;k<this.w.length;k++) { this.w[k] += V.w[k]; }}
+    addFromScaled(V, a) { for(let k=0;k<this.w.length;k++) { this.w[k] += a*V.w[k]; }}
+    setConst(a) { for(let k=0;k<this.w.length;k++) { this.w[k] = a; }}
 
-    toJSON: function() {
+    toJSON() {
       // todo: we may want to only save d most significant digits to save space
-      var json = {}
+      const json = {}
       json.sx = this.sx; 
       json.sy = this.sy;
       json.depth = this.depth;
       json.w = Array.prototype.slice.call(this.w);
       return json;
       // we wont back up gradients to save space
-    },
-    fromJSON: function(json) {
+    }
+    fromJSON(json) {
       this.sx = json.sx;
       this.sy = json.sy;
       this.depth = json.depth;
 
-      var n = this.sx*this.sy*this.depth;
+      const n = this.sx*this.sy*this.depth;
       this.w = zeros(n);
       this.dw = zeros(n);
       // copy over the elements.
-      for(var i=0;i<n;i++) {
+      for(let i=0;i<n;i++) {
         this.w[i] = json.w[i];
       }
     }
   }
 
   export { Vol };
-
--- src/convnet_vol_util.js
+++ src/convnet_vol_util.js
@@ -6,20 +6,19 @@ import { randi } from './convnet_util.js';
   // crop is the size of output
   // dx,dy are offset wrt incoming volume, of the shift
   // fliplr is boolean on whether we also want to flip left<->right
-  var augment = function(V, crop, dx, dy, fliplr) {
+  function augment(V, crop, dx, dy, fliplr = false) {
     // note assumes square outputs of size crop x crop
-    if(typeof(fliplr)==='undefined') var fliplr = false;
-    if(typeof(dx)==='undefined') var dx = randi(0, V.sx - crop);
-    if(typeof(dy)==='undefined') var dy = randi(0, V.sy - crop);
+    if(dx === undefined) dx = randi(0, V.sx - crop);
+    if(dy === undefined) dy = randi(0, V.sy - crop);
     
     // randomly sample a crop in the input volume
-    var W;
+    let W;
     if(crop !== V.sx || dx!==0 || dy!==0) {
       W = new Vol(crop, crop, V.depth, 0.0);
-      for(var x=0;x<crop;x++) {
-        for(var y=0;y<crop;y++) {
+      for(let x=0;x<crop;x++) {
+        for(let y=0;y<crop;y++) {
           if(x+dx<0 || x+dx>=V.sx || y+dy<0 || y+dy>=V.sy) continue; // oob
-          for(var d=0;d<V.depth;d++) {
+          for(let d=0;d<V.depth;d++) {
            W.set(x,y,d,V.get(x+dx,y+dy,d)); // copy data over
           }
         }
@@ -30,10 +29,10 @@ import { randi } from './convnet_util.js';
 
     if(fliplr) {
       // flip volume horziontally
-      var W2 = W.cloneAndZero();
-      for(var x=0;x<W.sx;x++) {
-        for(var y=0;y<W.sy;y++) {
-          for(var d=0;d<W.depth;d++) {
+      const W2 = W.cloneAndZero();
+      for(let x=0;x<W.sx;x++) {
+        for(let y=0;y<W.sy;y++) {
+          for(let d=0;d<W.depth;d++) {
            W2.set(x,y,d,W.get(W.sx - x - 1,y,d)); // copy data over
           }
         }
@@ -45,14 +44,12 @@ import { randi } from './convnet_util.js';
 
   // img is a DOM element that contains a loaded image
   // returns a Vol of size (W, H, 4). 4 is for RGBA
-  var img_to_vol = function(img, convert_grayscale) {
+  function img_to_vol(img, convert_grayscale = false) {
 
-    if(typeof(convert_grayscale)==='undefined') var convert_grayscale = false;
-
-    var canvas = document.createElement('canvas');
+    const canvas = document.createElement('canvas');
     canvas.width = img.width;
     canvas.height = img.height;
-    var ctx = canvas.getContext("2d");
+    const ctx = canvas.getContext("2d");
 
     // due to a Firefox bug
     try {
@@ -66,8 +63,9 @@ import { randi } from './convnet_util.js';
       }
     }
 
+    let img_data;
     try {
-      var img_data = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      img_data = ctx.getImageData(0, 0, canvas.width, canvas.height);
     } catch (e) {
       if(e.name === 'IndexSizeError') {
         return false; // not sure what causes this sometimes but okay abort
@@ -77,21 +75,21 @@ import { randi } from './convnet_util.js';
     }
 
     // prepare the input: get pixels and normalize them
-    var p = img_data.data;
-    var W = img.width;
-    var H = img.height;
-    var pv = []
-    for(var i=0;i<p.length;i++) {
+    const p = img_data.data;
+    const W = img.width;
+    const H = img.height;
+    const pv = []
+    for(let i=0;i<p.length;i++) {
       pv.push(p[i]/255.0-0.5); // normalize image pixels to [-0.5, 0.5]
     }
-    var x = new Vol(W, H, 4, 0.0); //input volume (image)
+    let x = new Vol(W, H, 4, 0.0); //input volume (image)
     x.w = pv;
 
     if(convert_grayscale) {
       // flatten into depth=1 array
-      var x1 = new Vol(W, H, 1, 0.0);
-      for(var i=0;i<W;i++) {
-        for(var j=0;j<H;j++) {
+      const x1 = new Vol(W, H, 1, 0.0);
+      for(let i=0;i<W;i++) {
+        for(let j=0;j<H;j++) {
           x1.set(i,j,0,x.get(i,j,0));
         }
       }
