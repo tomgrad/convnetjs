140c28ece96106cedee1216a5d9a1d00076c0715
Tomasz Gradowski <tomgrad@gmail.com>
2026-09-16 01:14:28 +0200

    refactor: modernize convnet_util syntax

--- src/convnet_util.js
+++ src/convnet_util.js
@@ -1,63 +1,56 @@
   // Random number utilities
-  var return_v = false;
-  var v_val = 0.0;
-  var gaussRandom = function() {
+  let return_v = false;
+  let v_val = 0.0;
+  function gaussRandom() {
     if(return_v) { 
       return_v = false;
       return v_val; 
     }
-    var u = 2*Math.random()-1;
-    var v = 2*Math.random()-1;
-    var r = u*u + v*v;
+    const u = 2*Math.random()-1;
+    const v = 2*Math.random()-1;
+    const r = u*u + v*v;
     if(r == 0 || r > 1) return gaussRandom();
-    var c = Math.sqrt(-2*Math.log(r)/r);
+    const c = Math.sqrt(-2*Math.log(r)/r);
     v_val = v*c; // cache this
     return_v = true;
     return u*c;
   }
-  var randf = function(a, b) { return Math.random()*(b-a)+a; }
-  var randi = function(a, b) { return Math.floor(Math.random()*(b-a)+a); }
-  var randn = function(mu, std){ return mu+gaussRandom()*std; }
+  function randf(a, b) { return Math.random()*(b-a)+a; }
+  function randi(a, b) { return Math.floor(Math.random()*(b-a)+a); }
+  function randn(mu, std){ return mu+gaussRandom()*std; }
 
   // Array utilities
-  var zeros = function(n) {
-    if(typeof(n)==='undefined' || isNaN(n)) { return []; }
-    if(typeof ArrayBuffer === 'undefined') {
-      // lacking browser support
-      var arr = new Array(n);
-      for(var i=0;i<n;i++) { arr[i]= 0; }
-      return arr;
-    } else {
-      return new Float64Array(n);
-    }
+  function zeros(n) {
+    if(n === undefined || isNaN(n)) { return []; }
+    return new Float64Array(n);
   }
 
-  var arrContains = function(arr, elt) {
-    for(var i=0,n=arr.length;i<n;i++) {
+  function arrContains(arr, elt) {
+    for(let i=0,n=arr.length;i<n;i++) {
       if(arr[i]===elt) return true;
     }
     return false;
   }
 
-  var arrUnique = function(arr) {
-    var b = [];
-    for(var i=0,n=arr.length;i<n;i++) {
-      if(!arrContains(b, arr[i])) {
-        b.push(arr[i]);
+  function arrUnique(arr) {
+    const b = [];
+    for(const elt of arr) {
+      if(!arrContains(b, elt)) {
+        b.push(elt);
       }
     }
     return b;
   }
 
   // return max and min of a given non-empty array.
-  var maxmin = function(w) {
+  function maxmin(w) {
     if(w.length === 0) { return {}; } // ... ;s
-    var maxv = w[0];
-    var minv = w[0];
-    var maxi = 0;
-    var mini = 0;
-    var n = w.length;
-    for(var i=1;i<n;i++) {
+    let maxv = w[0];
+    let minv = w[0];
+    let maxi = 0;
+    let mini = 0;
+    const n = w.length;
+    for(let i=1;i<n;i++) {
       if(w[i] > maxv) { maxv = w[i]; maxi = i; } 
       if(w[i] < minv) { minv = w[i]; mini = i; } 
     }
@@ -65,12 +58,12 @@
   }
 
   // create random permutation of numbers, in range [0...n-1]
-  var randperm = function(n) {
-    var i = n,
+  function randperm(n) {
+    let i = n,
         j = 0,
         temp;
-    var array = [];
-    for(var q=0;q<n;q++)array[q]=q;
+    const array = [];
+    for(let q=0;q<n;q++)array[q]=q;
     while (i--) {
         j = Math.floor(Math.random() * (i+1));
         temp = array[i];
@@ -82,10 +75,10 @@
 
   // sample from list lst according to probabilities in list probs
   // the two lists are of same size, and probs adds up to 1
-  var weightedSample = function(lst, probs) {
-    var p = randf(0, 1.0);
-    var cumprob = 0.0;
-    for(var k=0,n=lst.length;k<n;k++) {
+  function weightedSample(lst, probs) {
+    const p = randf(0, 1.0);
+    let cumprob = 0.0;
+    for(let k=0,n=lst.length;k<n;k++) {
       cumprob += probs[k];
       if(p < cumprob) { return lst[k]; }
     }
@@ -93,15 +86,15 @@
   }
 
   // syntactic sugar function for getting default parameter values
-  var getopt = function(opt, field_name, default_value) {
+  function getopt(opt, field_name, default_value) {
     if(typeof field_name === 'string') {
       // case of single string
       return (typeof opt[field_name] !== 'undefined') ? opt[field_name] : default_value;
     } else {
       // assume we are given a list of string instead
-      var ret = default_value;
-      for(var i=0;i<field_name.length;i++) {
-        var f = field_name[i];
+      let ret = default_value;
+      for(let i=0;i<field_name.length;i++) {
+        const f = field_name[i];
         if (typeof opt[f] !== 'undefined') {
           ret = opt[f]; // overwrite return value
         }
